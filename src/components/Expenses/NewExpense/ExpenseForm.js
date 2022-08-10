7c83f0c9119c74ae9f1f7163e445967640ec1990
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2022-08-10 09:17:16 +0300

    toggle ExpenseForm to show or not

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -5,37 +5,19 @@ const ExpenseForm = ({ onNewExpense }) => {
   const [titleField, updateTitleField] = useState("");
   const [amountField, updateAmountField] = useState("");
   const [dateField, updateDateField] = useState("");
-  //   const [inputFields, updateInputFields] = useState({
-  //     titleField: "",
-  //     amountField: "",
-  //     dateField: "",
-  //   });
+
+  const [newExpenseField, updateNewExpenseField] = useState(false);
 
   const getTitleInput = (event) => {
     updateTitleField(event.target.value);
-    // updateInputFields({
-    //   ...inputFields,
-    //   titleField: event.target.value,
-    // });
-    // updateInputFields((inputFields) => {
-    //   return { ...inputFields, titleField: event.target.value }; // <- if you need to keep the original state of all elements
-    // });
   };
 
   const getAmountInput = (event) => {
     updateAmountField(event.target.value);
-    // updateInputFields({
-    //   ...inputFields,
-    //   amountField: event.target.value,
-    // });
   };
 
   const getDateInput = (event) => {
     updateDateField(event.target.value);
-    // updateInputFields({
-    //   ...inputFields,
-    //   dateField: event.target.value,
-    // });
   };
 
   const sumbitHandler = (event) => {
@@ -50,42 +32,53 @@ const ExpenseForm = ({ onNewExpense }) => {
     updateTitleField("");
     updateAmountField("");
     updateDateField("");
+    updateNewExpenseField(false);
   };
 
-  return (
-    <form onSubmit={sumbitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input type="text" value={titleField} onChange={getTitleInput} />
+  const expenseFieldDisplay = () => {
+    newExpenseField
+      ? updateNewExpenseField(false)
+      : updateNewExpenseField(true);
+  };
+
+  if (newExpenseField) {
+    return (
+      <form onSubmit={sumbitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input type="text" value={titleField} onChange={getTitleInput} />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              value={amountField}
+              min="0.01"
+              step="0.01"
+              onChange={getAmountInput}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              value={dateField}
+              min="2019-01-01"
+              max="2022-12-31"
+              onChange={getDateInput}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={amountField}
-            min="0.01"
-            step="0.01"
-            onChange={getAmountInput}
-          />
+        <div className="new-expense__actions">
+          <button type="submit">Submit Expense</button>
+          <button onClick={expenseFieldDisplay}>Hide</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            value={dateField}
-            min="2019-01-01"
-            max="2022-12-31"
-            onChange={getDateInput}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Submit Expense</button>
-        <button type="reset">Reset Expense</button>
-      </div>
-    </form>
-  );
+      </form>
+    );
+  }
+
+  return <button onClick={expenseFieldDisplay}>Add new Expense</button>;
 };
 
 export default ExpenseForm;
